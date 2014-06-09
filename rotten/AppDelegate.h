0f144ccacac27f597e74d5f97f3263a82414df5f
Stephani Alves <steph@stephanis-pro.gateway.2wire.net>
2014-06-08 17:20:18 -0700

    Initial Commit

---
+++ rotten/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rotten
+//
+//  Created by Stephani Alves on 6/8/14.
+//  Copyright (c) 2014 stephanimoroni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rotten/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  rotten
+//
+//  Created by Stephani Alves on 6/8/14.
+//  Copyright (c) 2014 stephanimoroni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
